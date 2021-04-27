7e87b0035fbed72db644beea3b46e3132eec5cbe
hideinworld <nitol.duy@gmail.com>
2021-04-27 11:33:02 +0700

    up

---
+++ server/server/ManagePacket.cs
@@ -0,0 +1,21 @@
+﻿namespace Cờ_cá_ngựa
+{
+    class ManagePacket
+    {
+        public string msgtype { get; set; }// user room action 
+        public string msgcontent { get; set; }
+        public string msgtime { get; set; }
+
+        public ManagePacket() {
+            msgcontent = "";
+            msgtype = "";
+            msgtime = "";
+        }
+        public ManagePacket(string type,string msg,string time)
+        {
+            this.msgtype = type;
+            this.msgcontent = msg;
+            this.msgtime = time;
+        }
+    }
+}
